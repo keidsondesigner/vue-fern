f180b054a33f6248619f4b7a69b70f64668a2f35
Keidson Roby <keidsondesigner@gmail.com>
2024-01-25 14:54:28 -0400

    style: :lipstick: ajustes css navbar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../shared/views/HomeView.vue'
+import HomeView from '@/shared/views/HomeView.vue'
+import AboutView from '@/shared/views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,10 +13,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../shared/views/AboutView.vue')
+      component: AboutView
     },
     {
       path: '/counter',
@@ -24,6 +22,13 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../counter/views/CounterView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: () => {
+        console.log('Rota não existe!');
+        return { name: 'home' }
+      }
     }
   ]
 })
---
+++ src/shared/components/NavBar.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <nav>
+    <div class="logo">
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="25" height="25" />
+      <span>Composition Api</span>
+    </div>
+
+    <div class="links">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/counter">Counter</RouterLink>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+}
+
+.logo {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
